63dff566f9555ff1049b4bc8e81e7b1a22e2a235
Kirill Baldin <baldinkirill58@gmail.com>
2024-11-22 21:29:44 +0300

    add menu

---
+++ lib/presentation/pages/menu_page/menu_page.dart
@@ -0,0 +1,255 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Fonts',
+      theme: ThemeData(fontFamily: 'ComicSans'),
+      home: const Scaffold(
+        backgroundColor: Color(0xFFD8ECFF),
+        appBar: CustomAppBar(),
+        body: CustomBody(),
+      ),
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: const Color(0xFF4F81A3),
+      elevation: 0,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(15.0);
+}
+
+class CustomBody extends StatelessWidget {
+  const CustomBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(17.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Строка для кнопки поиска и кнопки аккаунта
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    // Действие для поиска мероприятия
+                  },
+                  icon: const Icon(
+                    Icons.search,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  label: const Text(
+                    'Найти мероприятие',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        const Color(0xFF4F81A3), // Цвет кнопки поиска
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10), // Промежуток между кнопками
+              // Кнопка перехода на аккаунт
+              ElevatedButton(
+                onPressed: () {
+                  // Действие для перехода к виджету аккаунта
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      const Color(0xFF4F81A3), // Цвет кнопки аккаунта
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(20),
+                ),
+                child: const Icon(Icons.person, color: Colors.white, size: 30),
+              ),
+            ],
+          ),
+          const SizedBox(
+              height: 20), // Расстояние между кнопкой поиска и контейнером
+          Container(
+            decoration: BoxDecoration(
+              color: const Color(0xFF4F81A3),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Wrap(
+                  alignment: WrapAlignment.center,
+                  children: [
+                    Text(
+                      'Список предстоящих мероприятий',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        // fontWeight: FontWeight.bold
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF4F81A3),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 10),
+                      Container(
+                        height: 200,
+                        child: ListView.builder(
+                          itemCount: 10,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Мероприятие ${index + 1}', // Название мероприятия
+                                    style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
+                                  ),
+                                  Text(
+                                    '${21 + index}.11.2024', // Дата мероприятия (пример)
+                                    style: const TextStyle(
+                                        fontSize: 16, color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                // Действие для создания мероприятия
+              },
+              child: const Text(
+                'Создать мероприятие',
+                style: TextStyle(fontSize: 18),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 16),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Действие для отображения "Мои Мероприятия"
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Мои',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        'мероприятия',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10), // Промежуток между кнопками
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Действие для отображения "Архив Мероприятий"
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Архив',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        'мероприятий',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
