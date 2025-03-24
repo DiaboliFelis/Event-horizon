93cf0cc7e371015f80b627c4b9ddb8787d2571c4
DiaboliFelis <alezak@sfedu.ru>
2025-03-25 00:00:29 +0300

    +login

--- lib/presentation/pages/profile_page/profile_page.dart
+++ lib/presentation/pages/profile_page/profile_page.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -9,9 +9,10 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   User? _user;
-  String _userName = '';
+  String _userName = ''; //  Мы больше не используем displayName
   String _userEmail = '';
   String _userImageUrl = '';
+  String _userLogin = ''; //  Добавляем переменную для логина
 
   @override
   void initState() {
@@ -19,17 +20,29 @@ class _ProfilePageState extends State<ProfilePage> {
     _loadUserData();
   }
 
-  // Загрузка данных пользователя из Firebase
   Future<void> _loadUserData() async {
     User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
       setState(() {
         _user = user;
-        _userName = user.displayName ?? 'Не указано';
-        _userEmail = user.email ?? 'Не указано';
-        _userImageUrl =
-            user.photoURL ?? ''; // Если есть фото, то показываем его
+        _userImageUrl = user.photoURL ?? '';
       });
+
+      //  Получаем данные пользователя из Firestore
+      DocumentSnapshot<Map<String, dynamic>> snapshot =
+          await FirebaseFirestore.instance
+              .collection('users') // Замени 'users' на имя своей коллекции
+              .doc(user.uid)
+              .get();
+
+      if (snapshot.exists) {
+        setState(() {
+          _userLogin = snapshot.data()?['login'] ??
+              'Не указано'; //  Получаем логин из Firestore (поле 'login')
+          _userName = snapshot.data()?['name'] ??
+              'Не указано'; //  Получаем имя пользователя из Firestore (поле 'name')
+        });
+      }
     }
   }
 
@@ -49,9 +62,7 @@ class _ProfilePageState extends State<ProfilePage> {
         ],
       ),
       body: _user == null
-          ? Center(
-              child:
-                  CircularProgressIndicator()) // Показываем индикатор загрузки, пока данные не подгрузились
+          ? Center(child: CircularProgressIndicator())
           : Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
@@ -66,7 +77,7 @@ class _ProfilePageState extends State<ProfilePage> {
                   ),
                   SizedBox(height: 20),
                   Text(
-                    _userName,
+                    _userLogin, //  Отображаем логин
                     style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
