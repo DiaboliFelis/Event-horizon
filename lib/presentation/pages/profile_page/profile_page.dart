40858e4c3c48774887fbb6bb73a009938e2620fc
Kirill Baldin <baldinkirill58@gmail.com>
2025-03-24 23:18:55 +0300

    profile page fixed

--- lib/presentation/pages/profile_page/profile_page.dart
+++ lib/presentation/pages/profile_page/profile_page.dart
@@ -2,111 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class UserProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFFD8ECFF),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            onPressed: () {
-              _showOptionsDialog(context);
-            },
-          ),
-        ],
-      ),
-      body: MyProfile(),
-    );
-  }
-
-  // Отображение диалогового окна
-  void _showOptionsDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Выберите действие"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ListTile(
-                leading: Icon(Icons.edit),
-                title: Text("Изменить имя"),
-                onTap: () {
-                  Navigator.pop(context);
-                  _showChangeNameDialog(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.photo),
-                title: Text("Изменить фото"),
-                onTap: () {
-                  Navigator.pop(context);
-                  _pickImageFromGallery(context);
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  // Отображение диалога изменения имени
-  void _showChangeNameDialog(BuildContext context) {
-    TextEditingController nameController = TextEditingController();
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Изменить имя"),
-          content: TextField(
-            controller: nameController,
-            decoration: InputDecoration(hintText: "Введите новое имя"),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text("Отмена"),
-            ),
-            TextButton(
-              onPressed: () {
-                String newName = nameController.text;
-                if (newName.isNotEmpty) {
-                  // Добавить логику сохранения
-                }
-                Navigator.pop(context);
-              },
-              child: Text("Сохранить"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  // Выбор фото из галереи
-  void _pickImageFromGallery(BuildContext context) async {
-    /*final ImagePicker picker = ImagePicker();
-    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
-    if (image != null) {
-      // Добавить логику сохранения
-    }*/
-  }
+  _ProfilePageState createState() => _ProfilePageState();
 }
 
-class MyProfile extends StatefulWidget {
-  @override
-  _MyProfileState createState() => _MyProfileState();
-}
-
-class _MyProfileState extends State<MyProfile> {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _loginController = TextEditingController();
+class _ProfilePageState extends State<ProfilePage> {
+  User? _user;
+  String _userName = '';
+  String _userEmail = '';
+  String _userImageUrl = '';
 
   @override
   void initState() {
@@ -114,89 +19,71 @@ class _MyProfileState extends State<MyProfile> {
     _loadUserData();
   }
 
+  // Загрузка данных пользователя из Firebase
   Future<void> _loadUserData() async {
     User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
-      DocumentSnapshot userData = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(user.uid)
-          .get();
-
-      // Приведение данных к Map<String, dynamic>
-      if (userData.exists) {
-        final data = userData.data() as Map<String, dynamic>;
-        setState(() {
-          _nameController.text = data['name'] ?? '';
-          _loginController.text = data['email'] ?? '';
-        });
-      }
+      setState(() {
+        _user = user;
+        _userName = user.displayName ?? 'Не указано';
+        _userEmail = user.email ?? 'Не указано';
+        _userImageUrl =
+            user.photoURL ?? ''; // Если есть фото, то показываем его
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xFFD8ECFF),
-      child: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                width: 250,
-                height: 250,
-                decoration: const BoxDecoration(
-                  color: Color(0xFF4F81A3),
-                  shape: BoxShape.circle,
-                ),
-                child: ClipOval(
-                  child: Image.asset(
-                    'assets/account_icon.png', // Путь для иконки
-                    width: 10,
-                    height: 10,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 100),
-              SizedBox(
-                width: 300,
-                child: TextField(
-                  controller: _nameController,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'Имя пользователя',
-                    hintStyle: const TextStyle(color: Colors.white70),
-                    filled: true,
-                    fillColor: const Color(0xFF4F81A3),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: 300,
-                child: TextField(
-                  controller: _loginController,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'Логин',
-                    hintStyle: const TextStyle(color: Colors.white70),
-                    filled: true,
-                    fillColor: const Color(0xFF4F81A3),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                ),
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFD8ECFF),
+        title: Text('Профиль'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: () {
+              // Здесь можно добавить логику для изменения имени или фото
+            },
           ),
-        ),
+        ],
       ),
+      body: _user == null
+          ? Center(
+              child:
+                  CircularProgressIndicator()) // Показываем индикатор загрузки, пока данные не подгрузились
+          : Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundColor: Colors.grey[200],
+                    backgroundImage: _userImageUrl.isNotEmpty
+                        ? NetworkImage(_userImageUrl)
+                        : AssetImage('assets/account_icon.png')
+                            as ImageProvider,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    _userName,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    _userEmail,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  // Здесь можно добавить другие данные о пользователе, если они есть
+                ],
+              ),
+            ),
     );
   }
 }
