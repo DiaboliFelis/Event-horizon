d752e42712c372db38ee103acf23e48aeba7d2d9
DiaboliFelis <alezak@sfedu.ru>
2025-03-25 22:25:55 +0300

    + русский язык

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:event_horizon/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:event_horizon/presentation/Navigation.dart'; // Импорт вашего класса Navigation
+import 'package:flutter_localizations/flutter_localizations.dart'; // Импорт пакета
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp( options: DefaultFirebaseOptions.currentPlatform, );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,15 +18,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Event Horizon', // Замените на ваше название приложения
+      title: 'Event Horizon',
       theme: ThemeData(
-        primarySwatch: Colors.blue, // Или ваш preferred цвет
-        fontFamily: 'Pacifico'
+        primarySwatch: Colors.blue,
+        fontFamily: 'Pacifico',
       ),
       // Инициализация навигации
       onGenerateRoute: Navigation.generateRoute,
-      initialRoute: '/', // Запуск с SplashScreena
-      debugShowCheckedModeBanner: false, // Уберите баннер отладки
+      initialRoute: '/',
+      debugShowCheckedModeBanner: false,
+      // Добавьте следующие строки:
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations
+            .delegate, // Если используете Cupertino (iOS)
+      ],
+      supportedLocales: [
+        const Locale('ru', 'RU'), // Русский язык
+        // Добавьте другие языки, которые вы хотите поддерживать
+        // const Locale('en', 'US'), // Английский (США)
+      ],
     );
   }
 }
