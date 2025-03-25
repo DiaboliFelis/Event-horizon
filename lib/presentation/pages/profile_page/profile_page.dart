41df3ba380d9223630d4a0f06d91d49cec65e013
DiaboliFelis <alezak@sfedu.ru>
2025-03-25 22:48:10 +0300

    fix login

--- lib/presentation/pages/profile_page/profile_page.dart
+++ lib/presentation/pages/profile_page/profile_page.dart
@@ -46,53 +46,162 @@ class _ProfilePageState extends State<ProfilePage> {
     }
   }
 
+  // Отображение диалогового окна
+  void _showOptionsDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Выберите действие"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              /*ListTile(
+                leading: Icon(Icons.edit),
+                title: Text("Изменить имя"),
+                onTap: () {
+                  Navigator.pop(context);
+                  _showChangeNameDialog(context);
+                },
+              ),*/
+              ListTile(
+                leading: Icon(Icons.photo),
+                title: Text("Изменить фото"),
+                onTap: () {
+                  Navigator.pop(context);
+                  _pickImageFromGallery(context);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // Отображение диалога изменения имени
+  /*void _showChangeNameDialog(BuildContext context) {
+    TextEditingController nameController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Изменить имя"),
+          content: TextField(
+            controller: nameController,
+            decoration: InputDecoration(hintText: "Введите новое имя"),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Отмена"),
+            ),
+            TextButton(
+              onPressed: () {
+                String newName = nameController.text;
+                if (newName.isNotEmpty) {
+                  // добавить логику сохранения
+                }
+                Navigator.pop(context);
+              },
+              child: Text("Сохранить"),
+            ),
+          ],
+        );
+      },
+    );
+  }*/
+
+  // Выбор фото из галереи
+  void _pickImageFromGallery(BuildContext context) async {
+    /*final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      // добавить логику сохранения
+    }*/
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFD8ECFF),
       appBar: AppBar(
         backgroundColor: const Color(0xFFD8ECFF),
-        title: Text('Профиль'),
+        title: const Text('Профиль'),
         actions: [
           IconButton(
-            icon: Icon(Icons.edit),
+            icon: const Icon(Icons.edit),
             onPressed: () {
-              // Здесь можно добавить логику для изменения имени или фото
+              _showOptionsDialog(context);
             },
           ),
         ],
       ),
       body: _user == null
-          ? Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    radius: 80,
-                    backgroundColor: Colors.grey[200],
-                    backgroundImage: _userImageUrl.isNotEmpty
-                        ? NetworkImage(_userImageUrl)
-                        : AssetImage('assets/account_icon.png')
-                            as ImageProvider,
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    _userLogin, //  Отображаем логин
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 250,
+                      height: 250,
+                      margin: const EdgeInsets.only(top: 50),
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF4F81A3),
+                        shape: BoxShape.circle,
+                      ),
+                      child: ClipOval(
+                        child: _userImageUrl.isNotEmpty
+                            ? Image.network(
+                                _userImageUrl,
+                                width: 250,
+                                height: 250,
+                                fit: BoxFit.cover,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return Image.asset(
+                                    'assets/account_icon.png',
+                                    width: 250,
+                                    height: 250,
+                                    fit: BoxFit.cover,
+                                  );
+                                },
+                              )
+                            : Image.asset(
+                                'assets/account_icon.png', // Путь для иконки
+                                width: 250,
+                                height: 250,
+                                fit: BoxFit.cover,
+                              ),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    _userEmail,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
+                    const SizedBox(height: 50),
+                    SizedBox(
+                      width: 300,
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        readOnly: true,
+                        decoration: InputDecoration(
+                          hintText: _userLogin, //  Имя пользователя
+                          hintStyle: TextStyle(
+                            color: Color.fromARGB(179, 15, 14, 14),
+                            fontSize: 25,
+                          ),
+                          filled: true,
+                          fillColor: const Color(0xFF4F81A3),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                  // Здесь можно добавить другие данные о пользователе, если они есть
-                ],
+                    const SizedBox(height: 20),
+                  ],
+                ),
               ),
             ),
     );
