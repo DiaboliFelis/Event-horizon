341cc62089b0368ae8cbdf77f2355e911b0afbeb
DiaboliFelis <alezak@sfedu.ru>
2025-05-04 05:08:19 +0300

    + working links in the vishlist

--- lib/presentation/pages/wishlist_page/wishlist_page.dart
+++ lib/presentation/pages/wishlist_page/wishlist_page.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart'; // Добавьте эту зависимость
+import 'package:flutter/services.dart';
 
 class WishlistListScreen extends StatefulWidget {
   final String documentId;
@@ -57,6 +59,48 @@ class wishlistListScreenState extends State<WishlistListScreen> {
     }
   }
 
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+      throw 'Could not launch $url';
+    }
+  }
+
+  void _copyToClipboard(String text) {
+    Clipboard.setData(ClipboardData(text: text));
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Ссылка скопирована в буфер обмена')),
+    );
+  }
+
+  String? _extractURL(String text) {
+    // Функция для извлечения URL из текста
+    final RegExp urlRegExp = RegExp(
+        r'(?:(?:https?|ftp):\/\/)?[\w.-]+\.[\w.-]+(?:[/[\w-._~:/?#[\]@!$&\()*+,;=.]*)?'); // Более точное регулярное выражение
+
+    final match = urlRegExp.firstMatch(text);
+    if (match != null) {
+      return text.substring(match.start, match.end); // Возвращаем найденный URL
+    }
+    return null; // Если URL не найден, возвращаем null
+  }
+
+  String _extractText(String text, String? url) {
+    // Извлекаем текст до и после URL
+    if (url == null) {
+      return text;
+    }
+    int urlIndex = text.indexOf(url);
+    if (urlIndex == -1) {
+      return text;
+    }
+
+    String textBefore = text.substring(0, urlIndex);
+    String textAfter = text.substring(urlIndex + url.length);
+
+    return '$textBefore $url $textAfter'; // Добавляем пробелы вокруг URL
+  }
+
   void _showAddFoodDialog() {
     final TextEditingController controller = TextEditingController();
 
@@ -72,6 +116,7 @@ class wishlistListScreenState extends State<WishlistListScreen> {
           content: TextField(
             controller: controller,
             decoration: const InputDecoration(labelText: 'Введите желание'),
+            style: Theme.of(context).textTheme.bodyLarge,
           ),
           actions: [
             TextButton(
@@ -96,6 +141,7 @@ class wishlistListScreenState extends State<WishlistListScreen> {
 
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -110,38 +156,98 @@ class wishlistListScreenState extends State<WishlistListScreen> {
 //            color: const Color(0x993C3C43),
 //             child: const Padding(
 //               padding: EdgeInsets.all(0),
-          child: Text('Вишлист', style: TextStyle(color: Colors.black)),
+          child: Text('Вишлист',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: theme.textTheme.bodyLarge?.fontFamily)),
         ),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  itemCount: wishlist.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: Card(
-                        color: const Color(0xFFD0E4F7),
-                        child: ListTile(
-                          title: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 15.0, horizontal: 10),
-                            child: Text(
-                              wishlist[index],
-                              style: const TextStyle(color: Colors.black),
+      body: DefaultTextStyle(
+        // Оборачиваем body в DefaultTextStyle
+        style: TextStyle(fontFamily: theme.textTheme.bodyLarge?.fontFamily),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: wishlist.length,
+                    itemBuilder: (context, index) {
+                      String wish = wishlist[index];
+                      String? url = _extractURL(wish);
+                      bool isURL = url != null;
+
+                      String textBefore = ""; // Инициализируем
+                      String textAfter = ""; // Инициализируем
+
+                      if (isURL && url != null) {
+                        int urlIndex = wish.indexOf(url); // Находим индекс URL
+                        textBefore =
+                            wish.substring(0, urlIndex).trim(); // Текст до URL
+                        textAfter = wish
+                            .substring(urlIndex + url.length)
+                            .trim(); // Текст после URL
+                      } else {
+                        textBefore = wish; // Если URL нет, то все - текст
+                      }
+
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Card(
+                          color: const Color(0xFFD0E4F7),
+                          child: ListTile(
+                            title: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 15.0, horizontal: 10),
+                              child: Wrap(
+                                // Теперь у нас Wrap содержит отдельные виджеты
+                                children: [
+                                  if (textBefore
+                                      .isNotEmpty) // Отображаем текст до URL, если он есть
+                                    Text(
+                                      '$textBefore ', // Добавляем пробел после текста
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  if (isURL &&
+                                      url !=
+                                          null) // Если есть URL, отображаем его
+                                    GestureDetector(
+                                      onTap: () {
+                                        _launchURL(url);
+                                      },
+                                      onLongPress: () {
+                                        _copyToClipboard(url);
+                                      },
+                                      child: Text(
+                                        url,
+                                        style: const TextStyle(
+                                          color: Colors.blue,
+                                          decoration: TextDecoration.underline,
+                                        ),
+                                      ),
+                                    ),
+                                  if (textAfter
+                                      .isNotEmpty) // Отображаем текст после URL, если он есть
+                                    Text(
+                                      ' $textAfter', // Добавляем пробел перед текстом
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -156,4 +262,45 @@ class wishlistListScreenState extends State<WishlistListScreen> {
       ),
     );
   }
+}
+
+// Функция для создания списка TextSpan
+List<TextSpan> _buildTextSpans(String text, String? url) {
+  List<TextSpan> spans = [];
+
+  if (url == null) {
+    // Если URL не найден, возвращаем один TextSpan со всем текстом
+    spans.add(TextSpan(text: text));
+    return spans;
+  }
+
+  int urlStartIndex = text.indexOf(url);
+  if (urlStartIndex == -1) {
+    // Если URL не найден (что-то пошло не так), возвращаем один TextSpan со всем текстом
+    spans.add(TextSpan(text: text));
+    return spans;
+  }
+
+  // Добавляем текст до URL
+  if (urlStartIndex > 0) {
+    spans.add(TextSpan(text: text.substring(0, urlStartIndex)));
+  }
+
+  // Добавляем URL с нужными стилями
+  spans.add(
+    TextSpan(
+      text: url,
+      style: const TextStyle(
+        color: Colors.blue,
+        decoration: TextDecoration.underline,
+      ),
+    ),
+  );
+
+  // Добавляем текст после URL
+  if (urlStartIndex + url.length < text.length) {
+    spans.add(TextSpan(text: text.substring(urlStartIndex + url.length)));
+  }
+
+  return spans;
 }
